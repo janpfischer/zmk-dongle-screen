0d0b36bce159323b2cbf3003161fe358d1ba4243
sergei <sergei.bukharov@tech.jago.com>
2025-08-12 17:15:52 +0800

    X will be displayed when keyboard is not connected

--- boards/shields/dongle_screen/src/widgets/battery_status.c
+++ boards/shields/dongle_screen/src/widgets/battery_status.c
@@ -43,8 +43,10 @@ static lv_color_t battery_image_buffer[ZMK_SPLIT_CENTRAL_PERIPHERAL_COUNT + SOUR
 
 static void draw_battery(lv_obj_t *canvas, uint8_t level, bool usb_present) {
     
-    if (level <= CONFIG_DONGLE_SCREEN_BATTERY_RED_LEVEL)
+    if (level < 1)
     {
+        lv_canvas_fill_bg(canvas, lv_palette_main(LV_PALETTE_RED), LV_OPA_COVER);
+    } else if (level <= CONFIG_DONGLE_SCREEN_BATTERY_RED_LEVEL) {
         lv_canvas_fill_bg(canvas, lv_palette_main(LV_PALETTE_RED), LV_OPA_COVER);
     } else if (level <= CONFIG_DONGLE_SCREEN_BATTERY_YELLOW_LEVEL) {
         lv_canvas_fill_bg(canvas, lv_palette_main(LV_PALETTE_YELLOW), LV_OPA_COVER);
@@ -92,8 +94,11 @@ static void set_battery_symbol(lv_obj_t *widget, struct battery_state state) {
         lv_label_set_text(label, "X");
     }
 
-    if (state.level <= CONFIG_DONGLE_SCREEN_BATTERY_RED_LEVEL)
+    if (state.level < 1)
     {
+        lv_obj_set_style_text_color(label, lv_palette_main(LV_PALETTE_RED), 0);
+        lv_label_set_text(label, "X");
+    } else if (state.level <= CONFIG_DONGLE_SCREEN_BATTERY_RED_LEVEL) {
         lv_obj_set_style_text_color(label, lv_palette_main(LV_PALETTE_RED), 0);
         lv_label_set_text_fmt(label, "%4u", state.level);
     } else if (state.level <= CONFIG_DONGLE_SCREEN_BATTERY_YELLOW_LEVEL) {
