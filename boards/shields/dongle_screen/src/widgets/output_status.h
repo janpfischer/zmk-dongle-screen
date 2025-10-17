f72f45fd8353ef06e9fd4707bf427941f82b4bd5
gnomadic <edflem+github@gmail.com>
2025-10-16 21:36:36 -0400

    Revert "new output status"
    This reverts commit 3ff92a00baae2dbdd6e85b3365e397433570ea37.

--- boards/shields/dongle_screen/src/widgets/output_status.h
+++ boards/shields/dongle_screen/src/widgets/output_status.h
@@ -10,13 +10,13 @@
 #include <zephyr/kernel.h>
 
 // output_status.h
-struct zmk_widget_output_status {
-    sys_snode_t node;
+struct zmk_widget_output_status
+{
     lv_obj_t *obj;
-    lv_obj_t *connection_label;  // Changed from transport_label and ble_label
+    lv_obj_t *transport_label;
+    lv_obj_t *ble_label;
+    sys_snode_t node;
 };
 
 int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status *widget);
-
-
